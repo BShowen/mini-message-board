c79e6abff0c121fc6c75f53d9a72fbd706d587e7
Bradley Showen <BShowen@me.com>
2022-11-19 09:00:20 -0500

    Add preliminary styling and test data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const messages = [
+    {
+      title: "My first message",
+      body: "The body of my first message",
+    },
+    {
+      title: "My second message",
+      body: "The body of my second message",
+    },
+    {
+      title: "My third message",
+      body: "The body of my third message",
+    },
+  ];
+  res.render("index", { messages });
 });
 
 module.exports = router;
