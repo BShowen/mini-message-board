2da1fdef1ac2b472c9320ff53741a7bd11bb2d29
Bradley Showen <BShowen@me.com>
2022-11-19 20:27:55 -0500

    Add route for new messages

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,44 @@
 var express = require("express");
 var router = express.Router();
 
+const options = {
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+};
+const dateTimeFormatter = new Intl.DateTimeFormat("en-US", options);
+
+const messages = [
+  {
+    author: "Edward",
+    body: "The body of my first message",
+    date: dateTimeFormatter.format(new Date()),
+  },
+  {
+    author: "Laila W.",
+    body: "The body of my second message",
+    date: dateTimeFormatter.format(new Date()),
+  },
+  {
+    author: "Erik",
+    body: "The body of my third message",
+    date: dateTimeFormatter.format(new Date()),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      title: "My first message",
-      body: "The body of my first message",
-    },
-    {
-      title: "My second message",
-      body: "The body of my second message",
-    },
-    {
-      title: "My third message",
-      body: "The body of my third message",
-    },
-  ];
   res.render("index", { messages });
+});
+
+router.post("/new", (req, res) => {
+  const newMessage = {
+    author: req.body["author"],
+    body: req.body["message-content"],
+    date: dateTimeFormatter.format(new Date()),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
 });
 
 module.exports = router;
